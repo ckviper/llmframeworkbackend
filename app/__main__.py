66fd861608facc11850c7dd3d0f9e4a19124efb4
Vyacheslav Samarin <slava.samarin.95@mail.ru>
2024-12-16 00:58:03 +0300

    add chat and message tables

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
 from app.database.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.database.database import Base
 
 class User(Base):
@@ -7,3 +9,27 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password_hash = Column(String, nullable=False)
+
+    chats = relationship("Chat", back_populates="user")
+    messages = relationship("Message", back_populates="user")
+
+class Chat(Base):
+    __tablename__ = "chats"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    context = Column(String)
+
+    user = relationship("User", back_populates="chats")
+    messages = relationship("Message", back_populates="chat")
+
+
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    chat_id = Column(Integer, ForeignKey("chats.id"), nullable=False)
+    content = Column(String)
+
+    user = relationship("User", back_populates="messages")
+    chat = relationship("Chat", back_populates="messages")
